b2ee11a04d946dc12e062c56b9f7b2876f16dbe9
Your Name <you@example.com>
2024-05-08 15:09:48 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -79,7 +79,12 @@ TreeNode * minimum(TreeNode * x){
 
 
 void removeNode(TreeMap * tree, TreeNode* node) {
-
+    if (tree == NULL || node == NULL) return;
+    if (node->left == NULL && node->right == NULL){
+        if (node->parent == NULL){
+            tree->root = NULL;
+        }
+    }
 }
 
 void eraseTreeMap(TreeMap * tree, void* key){
